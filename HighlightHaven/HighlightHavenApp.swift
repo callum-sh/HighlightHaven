237242bce663ef48a05f3743b4f4d52011676a4a
Callum Sharrock <38599348+callum-sh@users.noreply.github.com>
2025-01-01 21:24:24 -0700

    Initial Commit

---
+++ HighlightHaven/HighlightHavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighlightHavenApp.swift
+//  HighlightHaven
+//
+//  Created by callum on 2025-01-01.
+//
+
+import SwiftUI
+
+@main
+struct HighlightHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HighlightHavenTests/HighlightHavenTests.swift
@@ -0,0 +1,17 @@
+//
+//  HighlightHavenTests.swift
+//  HighlightHavenTests
+//
+//  Created by callum on 2025-01-01.
+//
+
+import Testing
+@testable import HighlightHaven
+
+struct HighlightHavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
